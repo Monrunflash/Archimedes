b7b302f4254a8eca0a55a3b583293506100d5283
Monrunflash <samuil.ggwp@gmail.com>
2022-03-30 22:57:42 +0200

    adding files

---
+++ word_analizer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import convert_text as ct
+
+def analizer(t):
+    wordSeparated = list()
+    wordSeparated = t.split()
+    text = list()
+    for w in wordSeparated:
+        segment = ct.punctuation_separator(w)
+        if isinstance(segment, list):
+            for p in segment:
+                text.append(p)
+        else:
+            text.append(w)
+    return text
