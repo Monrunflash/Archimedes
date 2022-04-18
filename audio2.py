709fe8e782c17b6a59d43b5effee85b25d07b89d
Monrunflash <samuil.ggwp@gmail.com>
2022-04-18 20:40:25 +0200

    mod

--- audio2.py
+++ audio2.py
@@ -1,13 +1,15 @@
-from tkinter import *
 import pygame
 import time
+import os
 
 pygame.mixer.init()
 
 def play(textToPlay):
     for words in textToPlay:
+        time.sleep(0.28)
         for syll in words:
-            print(syll)
-            pygame.mixer.music.load(f"Audacity/{syll}.mp3")
-            pygame.mixer.music.play(loops=0)
-            time.sleep(0.35)
+            path = f"Audacity/{syll}.mp3"
+            if os.path.isfile(path):
+                pygame.mixer.music.load(path)
+                pygame.mixer.music.play(loops=0)
+                time.sleep(0.28)
